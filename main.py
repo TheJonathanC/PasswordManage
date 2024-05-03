3afc8f74d324f96d1423923cfb78d59c3feb7113
DD <64969704+deiondz@users.noreply.github.com>
2024-05-04 01:45:24 +0530

    ADD

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
+from marshmallow import Schema, fields, ValidationError, validate
+import asyncio
+from prisma import Prisma, register
+db = Prisma()
+db.connect()
+register(db)
 
 app = Flask(__name__)
 
-@app.route('/login/<site_name>/<password>', methods=['POST'])
-def login(site_name, password):
+class LoginSchema(Schema):
+    site_name = fields.String(required=True, validate=validate.Length(min=1))
+    password = fields.String(required=True, validate=validate.Length(min=1))
 
-    return jsonify({'site_name': site_name, 'password': password})
+
+@app.route('/savecredentials', methods=['POST'])
+def login():
+  
+    try:
+        # Validate incoming JSON data against the LoginSchema
+        login_data = LoginSchema().load(request.json)
+        db.Password.create(
+        {
+            'sitename': login_data['site_name'],
+            'password': login_data['password']
+        }
+    )
+        print(login_data['site_name'], login_data['password'])
+        # Here you can access login_data['site_name'] and login_data['password'] for further processing
+        return jsonify({'success': True}), 200
+    except ValidationError as e:
+        # If validation fails, return error messages
+        return jsonify({'success': False, 'errors': e.messages}), 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
